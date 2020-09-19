6d222afe6828cf77e21aeab6cc116c913dae6153
kakapo0405 <kmnhm712712@gmail.com>
2020-09-19 20:07:55 +0900

    kicking off the test project

---
+++ index.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World By Vue.js!',
+        seen: true,
+        todos: [
+            {text: 'errands to run'},
+            {text: 'meeting people'},
+            {text: 'submit some documents'}
+        ]
+    }
+});
+
+app.message = "This world is nonsense";
+//app.seen = false;
